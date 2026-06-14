53556434f2bda460fca461a9f6784c3c2962727b
LizWise@github.com <elizabethmmudry@gmail.com>
2026-06-14 11:22:04 +0900

    Added Stylessheet and Finished main form element input sections

---
+++ script.js
@@ -0,0 +1,17 @@
+let data = [];
+
+const input = document.querySelector("#input");
+
+input.addEventListener("click", () => {
+    let info = prompt("Enter Information");
+
+    if (info === null) {
+        alert("No Information Was Received");
+    } else {
+        let 
+    }
+}
+
+)
+
+
